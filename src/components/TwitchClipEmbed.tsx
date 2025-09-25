62b51b409eb7aec9756b89d24c08d1b40b56891b
nflnkr <105123049+nflnkr@users.noreply.github.com>
2025-09-25 11:41:10 +0300

    add clip embed flashbang protection

--- src/components/TwitchClipEmbed.tsx
+++ src/components/TwitchClipEmbed.tsx
@@ -1,3 +1,5 @@
+import { motion } from "motion/react";
+
 import type { TwitchClipMetadata } from "~/model/twitch";
 
 const hostname = globalThis.location?.hostname;
@@ -13,11 +15,18 @@ export default function TwitchClipEmbed({ clip, autoplay, noChannels }: Props) {
     if (!clip || !hostname) return <p className="text-3xl">No clips</p>;
 
     return (
-        <iframe
+        <motion.iframe
             src={`${clip.embed_url}&parent=${hostname}&autoplay=${autoplay}`}
             allow="autoplay; picture-in-picture"
             allowFullScreen
             className="h-full w-full"
+            initial={{
+                opacity: 0,
+            }}
+            animate={{
+                opacity: 1,
+                transition: { delay: 0.7 },
+            }}
         />
     );
 }
